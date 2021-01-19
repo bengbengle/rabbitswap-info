0011b939a39283e735dd6aa12cf47ba390448353
bengbengle <bengbengle@gmail.com>
2021-01-19 18:35:38 +0800

    s

---
+++ precache-manifest.c0dfaca546d7fa701c44e9b7a5eb415c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fba08b5e15fd846994cb9442de67d6fd",
+    "url": "/rabbitswap-info/index.html"
+  },
+  {
+    "revision": "4ed1cc1aa5c2986d3f6c",
+    "url": "/rabbitswap-info/static/js/2.123d11f5.chunk.js"
+  },
+  {
+    "revision": "d00bc45cf23cd1efae5c7a8570e2d199",
+    "url": "/rabbitswap-info/static/js/2.123d11f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f50063e4e5e00916aee3",
+    "url": "/rabbitswap-info/static/js/main.5b3b88cd.chunk.js"
+  },
+  {
+    "revision": "5022e639c1debaf48a37",
+    "url": "/rabbitswap-info/static/js/runtime-main.252269f3.js"
+  },
+  {
+    "revision": "73dabb371ddce7c640334fe0904dcddf",
+    "url": "/rabbitswap-info/static/media/eth.73dabb37.png"
+  },
+  {
+    "revision": "51d2d0b1476461772e312715c7f5e8a3",
+    "url": "/rabbitswap-info/static/media/logo-white.51d2d0b1.png"
+  },
+  {
+    "revision": "16fc4e486a5292ca1b88b67b78102676",
+    "url": "/rabbitswap-info/static/media/logo.16fc4e48.png"
+  }
+]);
